d0b97c158f2ef1801b2b05b9354c5ef8ba1273b1
markeastman <meastman@redhat.com>
2017-08-21 10:33:34 +0100

    Created some users for disabled, locked, expired testing. Put a list of users on the test login page.

--- example/src/main/java/uk/me/eastmans/sap/b1/mock/MockData.java
+++ example/src/main/java/uk/me/eastmans/sap/b1/mock/MockData.java
@@ -19,6 +19,12 @@ public class MockData {
         users.put( u1.getName(), u1 );
         SapUser u2 = new SapUser("steve", true, true, false, true);
         users.put( u2.getName(), u2 );
+        SapUser u3 = new SapUser("disabled", false, true, true, true);
+        users.put( u3.getName(), u3 );
+        SapUser u4 = new SapUser("expired", true, false, true, true);
+        users.put( u4.getName(), u4 );
+        SapUser u5 = new SapUser("locked", true, true, true, false);
+        users.put( u5.getName(), u5 );
 
         u1.addAllowedCompany( new SapCompany( "C1","Company 1", "Long name for company 1", "ROLE_USERS", "ROLE_ROLE_USERS", ADMIN, NEW_ORDERS, USERS, FEATURE_4, FEATURE_5, FEATURE_6 ) );
         u1.addAllowedCompany( new SapCompany( "C2","Company 2", "Long name for company 2",ADMIN, USERS) );
@@ -26,6 +32,11 @@ public class MockData {
         u1.addAllowedCompany( new SapCompany( "C4","Company 4", "Long name for company 4",ADMIN, NEW_ORDERS, FEATURE_4, FEATURE_5 ) );
         u2.addAllowedCompany( new SapCompany( "C1","Company 1", "Long name for company 1",NEW_ORDERS, USERS) );
         u2.addAllowedCompany( new SapCompany( "C3","Company 3", "Long name for company 3",FEATURE_4, FEATURE_5, FEATURE_6 ) );
+
+        SapCompany placeHolder = new SapCompany("--","Invalid", "No companies available");
+        u3.addAllowedCompany(placeHolder);
+        u4.addAllowedCompany(placeHolder);
+        u5.addAllowedCompany(placeHolder);
     }
 
     public Map<String, SapUser> getUsers() {
